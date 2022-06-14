cb1085c12c74d4a9023ca2797408b1d2bd22070a
Zoljargal Altangerel <zoljargalaltangerel@Zoljargals-MacBook-Pro.local>
2022-06-14 16:23:25 +0800

    Updates

---
+++ precache-manifest.ae31f173e0d236ab6e392fef59e46889.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "288d7c74df4a29f66b33",
+    "url": "/static/css/main.353b21c0.chunk.css"
+  },
+  {
+    "revision": "288d7c74df4a29f66b33",
+    "url": "/static/js/main.288d7c74.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "3fc729cbfc51c32a0490",
+    "url": "/static/js/2.3fc729cb.chunk.js"
+  },
+  {
+    "revision": "6ee34327ee87b8f52b89",
+    "url": "/static/js/runtime~main.6ee34327.js"
+  },
+  {
+    "revision": "133f8ba664e46aa053f686c2e6715e50",
+    "url": "/static/media/calendar3.133f8ba6.png"
+  },
+  {
+    "revision": "9e2699fc164347d507daee570ca13e25",
+    "url": "/static/media/home_image.9e2699fc.png"
+  },
+  {
+    "revision": "5732ac1927cf86eea282c382a48e182a",
+    "url": "/static/media/culture.5732ac19.png"
+  },
+  {
+    "revision": "bc20d72aa15e3a4f2bfc87b12a134007",
+    "url": "/static/media/science.bc20d72a.png"
+  },
+  {
+    "revision": "7782bfcb04ec1204575cb521d25d2b07",
+    "url": "/static/media/art.7782bfcb.png"
+  },
+  {
+    "revision": "e23101756b464b5b2406e09745636d67",
+    "url": "/static/media/education.e2310175.png"
+  },
+  {
+    "revision": "f183b7121efa66c6dc8d9cd671d0bd2d",
+    "url": "/static/media/calendar1.f183b712.png"
+  },
+  {
+    "revision": "e78910389f97c180dfbba34071c0caf7",
+    "url": "/static/media/calendar2.e7891038.png"
+  },
+  {
+    "revision": "813ba1791007ee992ad6666e176fa4ed",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web/precache-manifest.340f37f16fe287af8a159b2d553c6c9c.js"
+  "/precache-manifest.ae31f173e0d236ab6e392fef59e46889.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/web/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
