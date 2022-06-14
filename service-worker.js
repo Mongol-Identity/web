ee6bd6b9619b381836dc5d1d5c73bd5c44ec7a28
Zoljargal Altangerel <zoljargalaltangerel@Zoljargals-MacBook-Pro.local>
2022-06-14 16:25:49 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae31f173e0d236ab6e392fef59e46889.js"
+  "/web/precache-manifest.340f37f16fe287af8a159b2d553c6c9c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
