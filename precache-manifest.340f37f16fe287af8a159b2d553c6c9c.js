5af22118153176b67d09af60b7c3989bfdd035fc
Zoljargal Altangerel <zoljargalaltangerel@Zoljargals-MacBook-Pro.local>
2022-06-14 14:04:43 +0800

    Updates

---
+++ precache-manifest.340f37f16fe287af8a159b2d553c6c9c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "288d7c74df4a29f66b33",
+    "url": "/web/static/css/main.353b21c0.chunk.css"
+  },
+  {
+    "revision": "288d7c74df4a29f66b33",
+    "url": "/web/static/js/main.288d7c74.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/web/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "3fc729cbfc51c32a0490",
+    "url": "/web/static/js/2.3fc729cb.chunk.js"
+  },
+  {
+    "revision": "1f824dfc142b1fa5cc02",
+    "url": "/web/static/js/runtime~main.1f824dfc.js"
+  },
+  {
+    "revision": "133f8ba664e46aa053f686c2e6715e50",
+    "url": "/web/static/media/calendar3.133f8ba6.png"
+  },
+  {
+    "revision": "9e2699fc164347d507daee570ca13e25",
+    "url": "/web/static/media/home_image.9e2699fc.png"
+  },
+  {
+    "revision": "5732ac1927cf86eea282c382a48e182a",
+    "url": "/web/static/media/culture.5732ac19.png"
+  },
+  {
+    "revision": "bc20d72aa15e3a4f2bfc87b12a134007",
+    "url": "/web/static/media/science.bc20d72a.png"
+  },
+  {
+    "revision": "7782bfcb04ec1204575cb521d25d2b07",
+    "url": "/web/static/media/art.7782bfcb.png"
+  },
+  {
+    "revision": "e23101756b464b5b2406e09745636d67",
+    "url": "/web/static/media/education.e2310175.png"
+  },
+  {
+    "revision": "f183b7121efa66c6dc8d9cd671d0bd2d",
+    "url": "/web/static/media/calendar1.f183b712.png"
+  },
+  {
+    "revision": "e78910389f97c180dfbba34071c0caf7",
+    "url": "/web/static/media/calendar2.e7891038.png"
+  },
+  {
+    "revision": "12863a4c61a62c880db0e27172e30ff6",
+    "url": "/web/index.html"
+  }
+];
